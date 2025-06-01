c01a3b1f1f7750d3ae0528f119518f85e379c739
Garegin1991 <inkognitoik@gmail.com>
2025-06-01 12:41:17 +0400

    added subs component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { FaFacebookF, FaInstagram, FaYoutube, FaTwitter } from 'react-icons/fa';
 import s from './Footer.module.scss';
 
 const Footer = () => {
-  return (
+  const [modal, setModal] = useState(false)
+
+  useEffect(() => {
+    if (modal) {
+      setTimeout(() => { setModal(false) }, 3000)
+    }
+  }, [modal])
+  return (<>
+    <div className="container">
+      <div className={s.subscribe}>
+        <h3>Subscribe</h3>
+        <h2>Join the fun. <br /> Subscribe now!</h2>
+        <p>
+          Subscribe to our newsletter for a weekly serving of recipes, cooking tips,
+          and exclusive insights straight to your inbox.
+        </p>
+
+        <div className={s.inputWrapper}>
+          <input type="email" placeholder="Email Address" />
+          <button onClick={() => setModal(true)}>Subscribe</button>
+        </div>
+      </div>
+    </div>
+    <div className={`${s.modal} ${modal ? s.active : ""}`}>Thank you for subscribing!</div>
     <footer className={s.footer}>
       <div className={s.leftSection}>
         <div className={s.logo}>
@@ -27,6 +50,7 @@ const Footer = () => {
         <FaTwitter className={s.icon} />
       </div>
     </footer>
+  </>
   );
 };
 
